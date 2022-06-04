2b712908d6bc2e70188c837828a4f566649565b0
Fede1998Elias <89209416+Fede1998Elias@users.noreply.github.com>
2022-06-04 19:10:19 -0300

    Sprint2

---
+++ java.js
@@ -0,0 +1,131 @@
+document.addEventListener('DOMContentLoaded', iniciarApp);
+
+let gastos = [];
+
+function iniciarApp() {
+    document.querySelector('#formulario').addEventListener('submit', enviarInforme);
+}
+
+function enviarInforme(e) {
+    e.preventDefault();
+
+    const texto = document.querySelector('#texto').value;
+    const numero = parseInt(document.querySelector('#numero').value);
+    
+    if (texto === '' || numero === 0) {
+        mostrarMensaje('Ingrese valores validos');
+        return;
+    }
+    agregarGastos(texto, numero);
+}
+
+function mostrarMensaje(mensaje) {
+    const existeError = document.querySelector('.errorMensaje');
+
+    if (!existeError) { 
+        const resultados = document.querySelector('#resultados');
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('errorMensaje');
+    
+        // Mensaje de error
+        divMensaje.textContent = mensaje;
+        resultados.appendChild(divMensaje);    
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
+}
+
+function agregarGastos(texto, numero) {
+    const objGastos = {
+        texto,
+        numero,
+        id: Date.now()
+    }
+    gastos.push(objGastos);
+    
+    mostrarGastos();
+}
+
+function mostrarGastos() {
+
+    limpiarHTML();
+    
+
+    gastos.forEach( gasto => {
+        const resultados = document.querySelector('#resultados');
+        
+        const {texto, numero, id} = gasto;
+        
+        //Vamos a crear un li
+        const nuevoGasto = document.createElement('LI');
+        nuevoGasto.className = 'estilosLi';
+        nuevoGasto.dataset.id = id;
+        
+        //Agregar el html del gasto
+        nuevoGasto.innerHTML = `
+        ${texto}: <span class="cantindadEstilo"> $ ${numero} </span>
+        `;
+
+        const btnEliminar = document.createElement('button');
+        btnEliminar.textContent = 'Eliminar'
+        btnEliminar.onclick = () => {
+            eliminarProducto(id);
+        }
+        
+        nuevoGasto.appendChild(btnEliminar);
+        //agregar html
+        resultados.appendChild(nuevoGasto);
+        
+    });
+    mostrarTotal(gastos);
+}
+
+function mostrarTotal(gastosTotales) {
+    let total = 0;
+    let cantidad = 0;
+
+    // Nos permite calcular el total a pagar
+    gastosTotales.forEach(articulo => {
+        total += articulo.numero;
+        cantidad++;
+    });
+    
+    mostrarHtml(total, cantidad);
+}
+
+function mostrarHtml(total = 0, cantidad = 0){
+    const resultados = document.querySelector('#resultados');
+    
+    const mostrarTotal = document.createElement('div');
+    mostrarTotal.classList.add('mostrarTotal');
+    mostrarTotal.textContent = 'Total: ';
+
+    const totalSpan = document.createElement('span');
+    totalSpan.classList.add('parrafoSpan');
+    totalSpan.textContent = `$${total}`;
+    
+    const mostrarPago = document.createElement('div');
+    mostrarPago.classList.add('mostrarTotales');
+    mostrarPago.textContent = 'A cada uno le toca aportar: ';
+
+    const pagoMostrar = document.createElement('span');
+    pagoMostrar.classList.add('parrafoSpan');
+    pagoMostrar.textContent = `$${total/cantidad}`;
+
+
+    mostrarTotal.appendChild(totalSpan);
+    mostrarPago.appendChild(pagoMostrar);
+    
+    resultados.appendChild(mostrarTotal);
+    resultados.appendChild(mostrarPago);
+}
+
+function limpiarHTML() {
+    const contenido = document.querySelector('#resultados');
+
+    while (contenido.firstChild) {
+        contenido.removeChild(contenido.firstChild);
+    }
+}
